17d8393e009bfaaee110363d2be3c8c7aa443f69
YaroslavDidkivskiy <yaroslavdidkivskiy@gmail.com>
2025-08-09 15:35:35 +0300

    implemented main.py with proper logic

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-# This is a sample Python script.
+import os
+import asyncio
+from scraping.scraper import scrape_dou_async
+from analysis.analyzer import load_from_json, count_tech_mentions, plot_top_tech
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+DATA_DIR = "data"
+CHART_DIR = "charts"
+os.makedirs(DATA_DIR, exist_ok=True)
+os.makedirs(CHART_DIR, exist_ok=True)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+async def main():
+    out_json = os.path.join(DATA_DIR, "dou_python_raw.json")
+    results = await scrape_dou_async({"search": "python"}, max_pages=3, save_json=out_json)
+
+    df = load_from_json(out_json)
+    tech_counts = count_tech_mentions(df)
+    print(tech_counts.head(20))
+
+    plot_path = os.path.join(CHART_DIR, "top_tech.png")
+    plot_top_tech(tech_counts, top_n=10, outpath=plot_path)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    asyncio.run(main())
