0057787762a4346b559fc429e600923b53a2610b
YaroslavDidkivskiy <yaroslavdidkivskiy@gmail.com>
2025-08-09 15:01:56 +0300

    implemented config.py for for flexibility and simplicity of the program

---
+++ config.py
@@ -0,0 +1,19 @@
+BASE_URL = "https://jobs.dou.ua"
+SEARCH_PATH = "/vacancies/"
+
+DEFAULT_SEARCH_PARAMS = {
+    "search": "python",
+    "page": 1,
+}
+
+TECH_KEYWORDS = [
+    "django", "flask", "fastapi", "sqlalchemy", "postgres", "postgresql",
+    "mysql", "mongodb", "redis", "celery", "rabbitmq",
+    "docker", "kubernetes", "aws", "gcp", "azure",
+    "pandas", "numpy", "scipy", "tensorflow", "pytorch",
+    "rest", "graphql", "aiohttp", "asyncio"
+]
+
+REQUEST_DELAY = 1.0
+
+MAX_PAGES = 5
